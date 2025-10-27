6213912590ffe73f54ffd3697d2033f5c420319c
ayshashermin <ayshashermin8@gmail.com>
2025-10-27 11:51:31 +0530

    logo upadate

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,9 +19,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
-// app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
-app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
-// app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
